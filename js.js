9917584df54d9f0252fc25a6c337b0e98379771b
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-04-07 21:46:42 -0300

    Algumas correções

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
-let imgLampada = document.querySelector(".img-lampada")
+// let imgLampadaAcesa = document.querySelector(".img-lampada-acesa").src = "img/lampada-acesa.jpg"
+// let imgLampadaApagada = document.querySelector(".img-lampada").src = "img/lampada-apagada.jpg"
 
+let spanImg = document.querySelector("#img")
 
 function ligar() {
-    
+   
 }
 
 function desligar() {
-
+    
 }
 
 function quebrar() {
