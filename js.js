03080e97b2f22298af4c5d7469a9da35f66a0313
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-04-10 21:09:08 -0300

    Acertando o código de js

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
-let foto = document.querySelector('#foto')
+// let foto = document.querySelector('#foto')
+// let img = document.querySelector('#img')
+
 let img = document.querySelector('#img')
 
 function ligar() {
-  
+    img.src = './img/lampada-acesa.jpg'
 }
 
 function desligar() {
-    
+    img.src = './img/lampada-apagada.jpg'
 }
 
 function quebrar() {
-
+    img.src = './img/lampada-quebrada.jpg'
 }
